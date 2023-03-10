67a1a2b28a7ffd8380dd0f62ed7c3628331c25be
jdparmar99@gmail.com <jdparmar99@gmail.com>
2023-03-11 00:01:38 +0530

    test1

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
-chng2
-qweqw
-qweqw
+output "fourthout" {
+  value = "fourthout"
+}
+
+output "fiftout" {
+  value = "fifth"
+}
+
+output "sixth" {
+  value = "sixth"
+}
+output "seventh" {
+  value = "seventh"
+}
+variable "username" {}
+output "printname" {
+  value = "hello, ${var.username}"
+}
